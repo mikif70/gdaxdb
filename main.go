245b3096e7f343407dd2188fe6d67b5553aa6242
Michele Fadda <mikif70@gmail.com>
2019-04-15 14:21:45 +0200

    +options

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/csv"
+	"flag"
 	"fmt"
 	"os"
 	"time"
@@ -10,34 +11,49 @@ import (
 )
 
 func main() {
+
+	flag.Parse()
+
 	cli := coinbasepro.NewClient()
 
 	//	pr, _ := cli.GetProducts()
 	//	fmt.Println(pr)
+	if parameters.Filename == "" {
+		panic("wrong filename")
+	}
 
-	file, err := os.Create(os.Args[1])
+	file, err := os.Create(parameters.Filename)
 	if err != nil {
 		fmt.Println(err)
 		panic(err)
 	}
 
-	start := time.Date(2018, 1, 1, 0, 0, 0, 0, time.UTC)
-	stop := start.Add(time.Hour * 24 * 10)
+	// RFC3339 = 2006-01-02T15:04:05Z
+	start, err := time.Parse(time.RFC3339, parameters.Start)
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
+
+	fmt.Printf("start: %s\n", start.String())
+	stop := start.Add(time.Duration(parameters.Granularity*300) * time.Second)
+	fmt.Printf("Stop: %s\n", stop.String())
+
 	wr := csv.NewWriter(file)
 
 	wr.Write([]string{"Time", "Volume", "Open", "Close", "High", "Low"})
 	wr.Flush()
 
-	for i := 1; i < 90; i++ {
+	for i := 1; i <= parameters.Iteration; i++ {
 		fmt.Println(start, stop)
 		params := coinbasepro.GetHistoricRatesParams{
 			Start:       start,
 			End:         stop,
-			Granularity: 3600,
+			Granularity: int(parameters.Granularity),
 		}
-		ret, err := cli.GetHistoricRates("ETC-EUR", params)
+		ret, err := cli.GetHistoricRates(parameters.Product, params)
 		start = stop
-		stop = stop.Add(time.Hour * 24 * 10)
+		stop = start.Add(time.Duration(parameters.Granularity*300) * time.Second)
 		if err != nil {
 			fmt.Println(err)
 			continue
@@ -64,7 +80,7 @@ func main() {
 			wr.Write(str)
 			wr.Flush()
 		}
-		time.Sleep(time.Second * 2)
+		time.Sleep(time.Second * 1)
 	}
 
 }
